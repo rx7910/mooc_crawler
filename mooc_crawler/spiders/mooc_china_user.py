b615fe225a0376741a6d78d1fd62bd471b2aa8f4
rx7910 <ren.xi@mioying.com>
2018-04-19 18:09:41 +0800

    user
    Signed-off-by: rx7910 <ren.xi@mioying.com>

--- mooc_crawler/spiders/mooc_china_user.py
+++ mooc_crawler/spiders/mooc_china_user.py
@@ -1,5 +1,4 @@
 import json
-
 import scrapy
 
 
@@ -9,7 +8,7 @@ class MoocChinaSpider(scrapy.Spider):
     # memberId
     user_info_url_pattern = 'https://www.icourse163.org/web/j/memberBean.getMocMemberPersonalDtoById.rpc?csrfKey=4e1578f4af174b65b9291cdd6acd1d92'
 
-    learn_statistic_url_pattern = 'https://www.icourse163.org/web/j/learnerCourseRpcBean.getPersonalLearningStatisticDto.rpc?csrfKey=4e1578f4af174b65b9291cdd6acd1d92'
+    # learn_statistic_url_pattern = 'https://www.icourse163.org/web/j/learnerCourseRpcBean.getPersonalLearningStatisticDto.rpc?csrfKey=4e1578f4af174b65b9291cdd6acd1d92'
 
     # uid: 4947184
     # pageIndex: 1
@@ -21,8 +20,8 @@ class MoocChinaSpider(scrapy.Spider):
     }
 
     current_user_id = None
-    user_info_data_object = None
-    user_learn_list = []
+    user_info_data_object_mapper = {}
+    user_learn_list_mapper = {}
 
     def start_requests(self):
 
@@ -40,67 +39,158 @@ class MoocChinaSpider(scrapy.Spider):
             self.current_user_id = user_id
 
             user_info_data = {
-                'menberId': str(user_id),
+                'memberId': str(user_id),
             }
 
-            user_info_request = scrapy.FormRequest(url=self.user_info_url_pattern, headers=self.headers, formdata=user_info_data, callback=self.parse_user_info)
+            meta_data = {
+                'userId': str(user_id),
+            }
+
+            user_info_request = scrapy.FormRequest(
+                meta=meta_data,
+                url=self.user_info_url_pattern,
+                headers=self.headers,
+                formdata=user_info_data,
+                callback=self.parse_user_info,
+            )
             yield user_info_request
-
-            print('after 1111111111111')
-
-            learn_list_data = {
-                'uid': str(user_id),
-                'pageIndex': '1',
-                'pageSize': '32',
-            }
-            learn_list_request = scrapy.FormRequest(url=self.learn_list_url_pattern, headers=self.headers, formdata=learn_list_data, callback=self.parse_learn_list)
-            yield learn_list_request
-
-            print('after 2222222222222')
-
-            yield {
-                'userInfo': self.user_info_data_object,
-                'userLearn': self.user_learn_list,
-            }
-
-            print('after 3333333333333')
-
-            self.user_info_data_object = None
-            self.user_learn_list = []
-
-    def parse(self, response):
-        resp = json.loads(response.body)
-
-        result = resp['result']
-
-        for course in result['result']:
-            # fetch commit here
-            school_short_cut = course['schoolPanel']['shortName']
-            id_num = course['id']
-            course_id = school_short_cut + str(id_num)
-            course_url = 'https://www.icourse163.org/course/course-' + course_id
-
-            yield course
 
     def parse_user_info(self, response):
         resp = json.loads(response.body)
-        self.user_info_data_object = resp['result']
+        user_id = response.meta['userId']
+
+        if str(resp['result']['memberId']) != user_id:
+            raise Exception('user_id doesn\'t match with response')
+        self.user_info_data_object_mapper[str(resp['result']['memberId'])] = resp['result']
+
+        learn_list_data = {
+            'uid': user_id or str(resp['result']['memberId']),
+            'pageIndex': '1',
+            'pageSize': '32',
+        }
+
+        meta_data = {
+            'userId': user_id,
+        }
+        learn_list_request = scrapy.FormRequest(
+            meta=meta_data,
+            url=self.learn_list_url_pattern,
+            headers=self.headers,
+            formdata=learn_list_data,
+            callback=self.parse_learn_list,
+        )
+        yield learn_list_request
 
     def parse_learn_list(self, response):
         resp = json.loads(response.body)
-        print('qqqqqqqqqqqqll', resp)
         result = resp['result']
-        print('rrrrrrrrrrrrrr', result)
-        self.user_learn_list.append(result['list'])
+        user_id = response.meta['userId']
 
-        if result['query']['totlePageCount'] > 1 and result['query']['totlePageCount'] != result['query']['indexPage']:
+        if user_id not in self.user_learn_list_mapper:
+            self.user_learn_list_mapper[user_id] = []
+
+        # insert data
+        if result['list'] is not None and len(result['list']) > 0:
+
+            if str(result['list'][0]['uid']) != user_id:
+                raise Exception('user_id does not match with response')
+
+            self.user_learn_list_mapper[user_id].append(result['list'])
+
+        # raise another request or yield data
+        if result['query']['totlePageCount'] > 1 and result['query']['totlePageCount'] != result['query']['pageIndex']:
             learn_list_data = {
-                'uid': str(self.current_user_id),
+                'uid': user_id or str(self.current_user_id),
                 'pageIndex': str(result['query']['pageIndex'] + 1),
                 'pageSize': '32',
             }
-            learn_list_request = scrapy.FormRequest(url=self.learn_list_url_pattern, headers=self.headers, formdata=learn_list_data, callback=self.parse_learn_list)
+            meta_data = {
+                'userId': user_id,
+            }
+            learn_list_request = scrapy.FormRequest(
+                url=self.learn_list_url_pattern,
+                headers=self.headers,
+                formdata=learn_list_data,
+                callback=self.parse_learn_list,
+                meta=meta_data,
+            )
             yield learn_list_request
+        else:
+            yield {
+                'userInfo': self.user_info_data_object_mapper[user_id],
+                'userLearn': self.user_learn_list_mapper[user_id],
+            }
+
+'''
+data format as follow:
+
+[
+    {
+        userInfo: {
+            department: null,
+            description: "",
+            followCount: 0,
+            followStatus: false,
+            followedCount: 0,
+            highestDegree: 5,
+            isTeacher: false,
+            jobName: null,
+            largeFaceUrl: "",
+            lectorTitle: null,
+            logoForCertUrl: null,
+            memberId: 1136787461,
+            memberType: 1,
+            nickName: "杨素素201709",
+            realName: null,
+            richDescription: null,
+            schoolId: null,
+            schoolName: null,
+            schoolShortName: null,
+            supportCommonMooc: null,
+            supportMooc: null,
+            supportPostgradexam: null,
+            supportSpoc: null,
+        },
+        userLearn: [
+            {
+                courseCoverUrl : "http://edu-image.nosdn.127.net/30CD34C01C7BBA7AABC0E04C8A39AFD3.jpg?imageView&thumbnail=510y288&quality=100",
+                courseId : 1002599007,
+                courseName : "工尺谱概论",
+                courseProductType : 1,
+                enrollCount : 1608,
+                mode : 0,
+                schoolId : 1001439001,
+                schoolName : "中央音乐学院",
+                schoolShortName : "CCOM",
+                supportCommonMooc : null,
+                termId : 1002781027,
+                uid : 1136787461,
+                whatCertGot : null,
+            },
+            {
+                ...   
+            },
+            {
+                ...   
+            },
+            {
+                ...   
+            },
+        ],
+    },
+    {
+        ...
+    },
+    {
+        ...
+    },
+    {
+        ...
+    },
+]
+
+'''
+
 
 
 
